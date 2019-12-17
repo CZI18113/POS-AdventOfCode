aaef9f3c6757bfd56acd3cabcaf5283181140c9b
Michael Czipke <CZI18113@spengergasse.at>
2019-12-17 14:00:39 +0100

    Readding Day1

---
+++ Day1/The Tyranny of the Rocket Equation/The Tyranny of the Rocket Equation/CalcFuel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Tyranny_of_the_Rocket_Equation
+{
+    class CalcFuel
+    {
+    }
+}
---
+++ Day1/The Tyranny of the Rocket Equation/The Tyranny of the Rocket Equation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace The_Tyranny_of_the_Rocket_Equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
