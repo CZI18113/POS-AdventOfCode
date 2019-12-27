3281967695c32847fbdfd32b0d1a7677f21fc5c4
Michael Czipke <CZI18113@spengergasse.at>
2019-12-27 15:47:36 +0100

    Correct Day1 First Part Solution

--- Day1/The Tyranny of the Rocket Equation/The Tyranny of the Rocket Equation/Program.cs
+++ Day1/The Tyranny of the Rocket Equation/The Tyranny of the Rocket Equation/Program.cs
@@ -2,11 +2,26 @@
 
 namespace The_Tyranny_of_the_Rocket_Equation
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] imput = System.IO.File.ReadAllLines("C:/users/micha/Desktop/AdventOfCode_POS/POS-AdventOfCode/Day1/PuzzleImput.txt");
+            Console.WriteLine("Total Fuel: " + CalcFuel(imput));
+            Console.ReadLine();
+        }
+
+        static int CalcFuel(string[] put)
+        {
+            int erg = 0;
+            int tempFuel = 0;
+            foreach (string s in put)
+            {
+                int.TryParse(s, out tempFuel);
+                int temp = (tempFuel / 3);
+                erg += temp - 2;
+            }
+            return erg;
         }
     }
 }
