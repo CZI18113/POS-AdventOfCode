ee13c05fb7ec43e8b7e6eeecf42967cc654066b1
Michael Czipke <CZI18113@spengergasse.at>
2019-12-29 18:54:45 +0100

    Day 1 Part 2 done

--- Day1/The Tyranny of the Rocket Equation/The Tyranny of the Rocket Equation/Program.cs
+++ Day1/The Tyranny of the Rocket Equation/The Tyranny of the Rocket Equation/Program.cs
@@ -8,6 +8,7 @@ namespace The_Tyranny_of_the_Rocket_Equation
         {
             string[] imput = System.IO.File.ReadAllLines("C:/users/micha/Desktop/AdventOfCode_POS/POS-AdventOfCode/Day1/PuzzleImput.txt");
             Console.WriteLine("Total Fuel: " + CalcFuel(imput));
+            Console.WriteLine("Total Fuel Part 2: " + CalcFuelCorrect(imput));
             Console.ReadLine();
         }
 
@@ -20,6 +21,24 @@ namespace The_Tyranny_of_the_Rocket_Equation
                 int.TryParse(s, out tempFuel);
                 int temp = (tempFuel / 3);
                 erg += temp - 2;
+            }
+            return erg;
+        }
+
+        static int CalcFuelCorrect(string[] put)
+        {
+            int erg = 0;
+            int tempFuel = 0;
+            // int temp = 5;
+            // int count = -1;
+            foreach (string s in put)
+            {
+                int.TryParse(s, out tempFuel);
+                while ((tempFuel / 3) - 2 > 0)
+                {
+                    tempFuel = (tempFuel / 3) - 2;
+                    erg += tempFuel;
+                }
             }
             return erg;
         }
