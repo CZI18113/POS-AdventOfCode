5776c18eb58bac070d39e226467f13f74b048590
Michael Czipke <CZI18113@spengergasse.at>
2020-01-01 19:05:58 +0100

    Day 3, solution init

---
+++ Day3/Crossed Wires/Crossed Wires/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Crossed_Wires
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
